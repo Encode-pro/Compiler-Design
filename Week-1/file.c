eb771a66b882acb38846219d08eb6b17aa28a8dc
Harshil Agarwal <77769646+Encode-pro@users.noreply.github.com>
2022-01-27 16:42:09 +0530

    Week-1

---
+++ Week-1/file.c
@@ -0,0 +1,8 @@
+#include <stdio.h> 
+void main ( ){
+int a , b , c ;
+scanf ( " %d %d " , & a , & b , & c ) ; int mul ;
+mul = a * b * c ;
+printf("Hello! World");
+printf ( " %d + 10 " , mul ) ;
+}
